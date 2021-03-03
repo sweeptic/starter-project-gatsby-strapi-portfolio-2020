3e8ddf04be1141008228dc307fac711443a350af
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-03-03 13:18:08 +0100

    6. hero image query

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,19 @@ import Image from "gatsby-image"
 import { Link } from "gatsby"
 import { graphql, useStaticQuery } from "gatsby"
 import SocialLinks from "../constants/socialLinks"
-// ...GatsbyImageSharpFluid
+
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      id
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 const Hero = () => {
   return <h2>hero component</h2>
