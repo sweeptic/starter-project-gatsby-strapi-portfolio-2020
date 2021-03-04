5745817ad176614f44641279fbbd46b917a3c8ef
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-03-04 12:17:46 +0100

    30. blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,43 @@
-import React from "react"
-import Layout from "../components/Layout"
-import { graphql } from "gatsby"
-import Blogs from "../components/Blogs"
-// ...GatsbyImageSharpFluid
+import React from 'react'
+import Layout from '../components/Layout'
+import { graphql } from 'gatsby'
+import Blogs from '../components/Blogs'
 
-const Blog = () => {
-  return <h2>blog page</h2>
+const Blog = ({
+  data: {
+    allStrapiBlogs: { nodes: blogs },
+  },
+}) => {
+  return (
+    <Layout>
+      <section className="blog-page">
+        <Blogs blogs={blogs} title="blog" />
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    allStrapiBlogs {
+      nodes {
+        slug
+        content
+        desc
+        date(formatString: "MMMM Do, YYYY")
+        id
+        title
+        category
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Blog
--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ export default ({ data }) => {
       <Services />
       <Jobs />
       <Projects projects={projects} title="featured projects" showLink />
-      <Blogs blogs={blogs} title="blog" showLink />
+      <Blogs blogs={blogs} title="latest articles" showLink />
     </Layout>
   )
 }
