1c0747b12eeac6b6d7378cabedfe9d4d9184401e
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-03-04 08:17:00 +0100

    21. projects setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,42 @@ import Jobs from '../components/Jobs'
 import Projects from '../components/Projects'
 import Blogs from '../components/Blogs'
 
-export default () => {
+export default ({ data }) => {
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data
+
   return (
     <Layout>
       <Hero />
       <Services />
       <Jobs />
+      <Projects projects={projects} title="featured projects" showLink />
     </Layout>
   )
 }
 
-// ...GatsbyImageSharpFluid
+const query = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        github
+        id
+        description
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          title
+        }
+      }
+    }
+  }
+`
