aebe993a330f60130473337dc48dfa055bfe631a
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-03-04 15:07:52 +0100

    46. formspree

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,12 +7,18 @@ const contact = () => {
       <section className="contact-page">
         <article className="contact-form">
           <h3>get in touch</h3>
-          <form>
+          <form action="https://formspree.io/f/mknpleap" method="POST">
             <div className="form-group">
-              <input type="text" placeholder="name" className="form-control" />
+              <input
+                type="text"
+                name="name"
+                placeholder="name"
+                className="form-control"
+              />
               <input
                 type="email"
                 placeholder="email"
+                name="email"
                 className="form-control"
               />
               <textarea
@@ -22,7 +28,9 @@ const contact = () => {
                 className="form-control"
               ></textarea>
             </div>
-            <button type="submit" className="submit-btn btn"></button>
+            <button type="submit" className="submit-btn btn">
+              submit here
+            </button>
           </form>
         </article>
       </section>
