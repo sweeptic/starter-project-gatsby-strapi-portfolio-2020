37807bed273f5248032084ab577715f8f3eff5b0
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-03-04 13:45:49 +0100

    37. footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,15 @@
-import React from "react"
+import React from 'react'
 // import "../css/main.css"
-import Navbar from "./Navbar"
-import Sidebar from "./Sidebar"
-import Footer from "./Footer"
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+import Footer from './Footer'
 
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
       {children}
+      <Footer />
     </>
   )
 }
