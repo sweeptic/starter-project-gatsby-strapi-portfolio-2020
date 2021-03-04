1aa84ffc8648a5a43123446f147608eafb66233e
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-03-04 12:07:25 +0100

    29. blog component

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,7 +57,7 @@ export const query = graphql`
         id
         title
         category
-        Image {
+        image {
           childImageSharp {
             fluid {
               ...GatsbyImageSharpFluid
